9750334917edff34a702a454c6c6a336fc930fe6
lapboi3 <74074061+ItsLap@users.noreply.github.com>
2021-04-17 17:19:48 +0100

    v0.5

--- public/script.js
+++ public/script.js
@@ -3,18 +3,20 @@ var messages = document.querySelector('#msgList');
 var form = document.querySelector("#msgForm")
 var input = document.querySelector('#msgInput');
 var username = document.querySelector('#msgName');
+var password = document.querySelector("#msgPassword")
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (input.value && username.value) {
-        socket.emit('message', username.value, input.value);
+        socket.emit('message', username.value, input.value, password.value);
         input.value = '';
     }
 });
 socket.on('message', (usr, msg) => {
-    var item = document.createElement('li');
+    var item = document.createElement('li')
     item.textContent = `${usr}: ${msg}`;
+    item.classList.add("list-group-item")
     messages.appendChild(item);
     document.querySelector("#scrollArea").scrollTo(0, document.body.scrollHeight);
 });
