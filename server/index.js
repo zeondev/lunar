25f138973902a17282bf17bb52431a91a880fc18
fwipsity <79366074+fwipsity@users.noreply.github.com>
2021-04-25 21:28:26 +0300

    fixed an empty user name bug
    fixed an empty user name bug

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get("/app", (req, res) => {
 
 io.on('connection', (socket) => {
     socket.on('message', (usr, msg, time) => {
-        if(msg.trim() === ""){
+        if(msg.trim() === "" || usr.trim() === ""){
             return
         }
         io.emit('message', usr, msg, time);
