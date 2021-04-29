ab87d6539cf96b5ab7956896a49df80d15937261
lapboi3 <74074061+ItsLap@users.noreply.github.com>
2021-04-29 17:47:00 +0100

    Update plugin.js

--- public/plugins/LunarEssentials/plugin.js
+++ public/plugins/LunarEssentials/plugin.js
@@ -1,6 +1,6 @@
 $("#addExtensionInput").val("https://raw.githubusercontent.com/fwipsity/lunar-extensions/main/spamBadge/plugin.js")
 extensionAdd()
-$("#addExtensionInput").val("/plugins/customtext")
+$("#addExtensionInput").val("/plugins/customtext/plugin.js")
 extensionAdd()
 
 addLocalMessage("Thanks for adding LunarEssentials!", "text-primary")
